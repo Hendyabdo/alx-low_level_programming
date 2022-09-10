c36360bf4d005f6f8f707400f80a673ffc49c145
Hendyabdo <abdohendo456@gmail.com>
2022-09-09 17:28:10 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char letetr;
+for( letetr='z'; letter >= 'a'; letter--)
+putchar(letetr)
+putchar('\n')
+return(0);
+}
