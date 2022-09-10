6e9b6017ddbc27ef2af4720ee4e531169b2afe3d
Hendyabdo <abdohendo456@gmail.com>
2022-09-09 17:29:09 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-char letetr;
-for( letetr='z'; letter >= 'a'; letter--)
-putchar(letetr)
+char letter;
+for( letter='z'; letter >= 'a'; letter--)
+putchar(letter)
 putchar('\n')
 return(0);
 }
