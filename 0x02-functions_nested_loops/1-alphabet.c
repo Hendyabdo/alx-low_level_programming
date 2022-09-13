b9a04dbaf8331c5c20f6ccbaa96d2db2c1a5be58
Hendyabdo <abdohendo456@gmail.com>
2022-09-13 08:19:19 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-char alpha;
-for ( alpha = 'a'; alpha <= 'z'; alpha ++)
-{
-_putchar(alpha);
-}
- _putchar ('\n');
-  
+  char alpha;
+
+  for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+      _putchar(alpha);
+    }
+  _putchar('\n');
 }
