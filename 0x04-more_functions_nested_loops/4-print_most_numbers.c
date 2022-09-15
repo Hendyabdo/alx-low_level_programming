293d118b79e265e5ae457ae4ca41453208211818
Hendyabdo <abdohendo456@gmail.com>
2022-09-15 07:51:02 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,15 @@
 void print_most_numbers(void)
 {
 int a;
-for ( a = 48; a <= 57; a++)
+for (a = 48; a < 58; a++)
 {
-if ( a == 50 || a == 52 )
-continue;
+if (a != 50)
+{
+if (a != 52)
+{
 _putchar(a);
+}
+}
 }
 _putchar('\n');
 }
